e9d405b15c670271b88dd3acffdcd37a9da5189c
Juancaceres963 <dnijuancaceres@gmail.com>
2024-07-31 13:48:53 -0300

    Solving react-bootstrps problems

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { NavBar } from 'c:/Users/USUARIO/personal-profolio/src/components/NavBar.js';
+import { NavBar } from './components/NavBar.js';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <NavBar/>
     </div>
   );
 }
