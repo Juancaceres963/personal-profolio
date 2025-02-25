0a6ffd8ec4356c5c3e90907041e93f182d797e8d
Juan Cáceres <dnijuancaceres@gmail.com>
2025-02-24 22:08:03 -0300

    Cleaning

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,10 +2,6 @@ import { Row, Col, Container } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import CircularProgress from "./CircularProgress";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
-import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
@@ -124,7 +120,7 @@ export const Skills = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-left" src={colorSharp} />
+      
     </section>
   );
 };
